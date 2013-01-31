9bc303a54691f3db890a95ad8d2fa72816b9eedd
Joachim Strömbergson <joachim@secworks.se>
2013-01-31 22:59:02 +0100

    Adding initial versions of the top level wrapper and associated testbench. The wrapper provides a simple 32-bit memory like interface for the SipHash core.

---
+++ src/siphash.v
@@ -0,0 +1,434 @@
+//======================================================================
+//
+// siphash_core.v
+// ---------------
+// Verilog 2001 implementation of SipHash.
+// This is the core with wide interface.
+//
+//
+// Copyright (c) 2012, Secworks Sweden AB
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or 
+// without modification, are permitted provided that the following 
+// conditions are met: 
+// 
+// 1. Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer. 
+// 
+// 2. Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in 
+//    the documentation and/or other materials provided with the 
+//    distribution. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//======================================================================
+
+module siphash(
+               // Clock and reset.
+               input wire           clk,
+               input wire           reset_n,
+
+               // Read and write interface.
+               input wire           cs,
+               input wire           wr_rd,
+               input wire [3 : 0]   addr,
+               input wire [31 : 0]  write_data,
+               output wire [31 : 0] read_data,
+               output wire          read_data_valid,
+               output wire          error
+              );
+
+  
+  //----------------------------------------------------------------
+  // Symbolic names.
+  //----------------------------------------------------------------
+  `include "siphash.vh"
+
+  
+  //----------------------------------------------------------------
+  // Registers including update variables and write enable.
+  //----------------------------------------------------------------
+  reg [2 : 0]  ctrl_reg;
+  reg [2 : 0]  ctrl_new;
+  reg          ctrl_we;
+  
+  reg [7 : 0]  param_reg;
+  reg [7 : 0]  param_new;
+  reg          param_we;
+
+  reg [31 : 0] key0_reg;
+  reg [31 : 0] key0_new;
+  reg          key0_we;
+
+  reg [31 : 0] key1_reg;
+  reg [31 : 0] key1_new;
+  reg          key1_we;
+
+  reg [31 : 0] key2_reg;
+  reg [31 : 0] key2_new;
+  reg          key2_we;
+
+  reg [31 : 0] key3_reg;
+  reg [31 : 0] key3_new;
+  reg          key3_we;
+
+  reg [31 : 0] mi0_reg;
+  reg [31 : 0] mi0_new;
+  reg          mi0_we;
+
+  reg [31 : 0] mi1_reg;
+  reg [31 : 0] mi1_new;
+  reg          mi1_we;
+
+  reg [31 : 0] word0_reg;
+  reg [31 : 0] word0_new;
+  reg          word0_we;
+
+  reg [31 : 0] word1_reg;
+  reg [31 : 0] word1_new;
+  reg          word1_we;
+  
+  
+  //----------------------------------------------------------------
+  // Wires.
+  //----------------------------------------------------------------
+  reg read_data_out;
+  reg read_data_valid_out;
+  reg error_out;
+
+  reg           core_initalize;
+  reg           core_compress;
+  reg           core_finalize;
+  reg [3 : 0]   core_c;
+  reg [3 : 0]   core_d;
+  reg [127 : 0] core_k;
+  reg [63 : 0]  core_mi;
+  wire          core_ready;
+  wire [63 : 0] core_siphash_word;
+  wire          core_siphash_word_valid;
+
+  
+  //----------------------------------------------------------------
+  // Concurrent connectivity for ports etc.
+  //----------------------------------------------------------------
+  assign read_data       = read_data_out;
+  assign read_data_valid = read_data_valid_out;
+  assign error           = error_out;
+
+
+  
+  //----------------------------------------------------------------
+  // Core instance.
+  //----------------------------------------------------------------
+  siphash_core core(
+                    .clk(clk),
+                    .reset_n(reset_n),
+                
+                    .initalize(core_initalize),
+                    .compress(core_compress),
+                    .finalize(core_finalize),
+
+                    .c(core_c),
+                    .d(core_d),
+                    .k(core_k),
+                    .mi(core_mi),
+
+                    .ready(core_ready),
+                    
+                    .siphash_word(core_siphash_word),
+                    .siphash_word_valid(core_siphash_word_valid)
+                   );
+  
+  
+  //----------------------------------------------------------------
+  // reg_update
+  // Update functionality for all registers in the core.
+  // All registers are positive edge triggered with synchronous
+  // active low reset. All registers have write enable.
+  //----------------------------------------------------------------
+  always @ (posedge clk)
+    begin
+      if (!reset_n)
+        begin
+          // Reset all registers to defined values.
+          ctrl_reg  <= 3'b000;
+          param_reg <= {SIPHASH_DEFAULT_D, SIPHASH_DEFAULT_C};
+          key0_reg  <= 32'h00000000;
+          key1_reg  <= 32'h00000000;
+          key2_reg  <= 32'h00000000;
+          key3_reg  <= 32'h00000000;
+          mi0_reg   <= 32'h00000000;
+          mi1_reg   <= 32'h00000000;
+          word0_reg <= 32'h00000000;
+          word1_reg <= 32'h00000000;
+        end
+      else
+        begin
+          if (ctrl_we)
+            begin
+              ctrl_reg <= ctrl_new;
+            end
+          
+          if (param_we)
+            begin
+              param_reg <= param_new;
+            end
+          
+          if (key0_we)
+            begin
+              key0_reg <= key0_new;
+            end
+          
+          if (key1_we)
+            begin
+              key1_reg <= key1_new;
+            end
+          
+          if (key2_we)
+            begin
+              key2_reg <= key2_new;
+            end
+          
+          if (key3_we)
+            begin
+              key3_reg <= key3_new;
+            end
+          
+          if (mi0_we)
+            begin
+              mi0_reg <= mi0_new;
+            end
+          
+          if (mi1_we)
+            begin
+              mi1_reg <= mi1_new;
+            end
+
+          // We sample the siphash word when valid is set.
+          if (core_siphash_word_valid)
+            begin
+              word0_reg <= core_siphash_word[31 : 0];
+              word1_reg <= core_siphash_word[63 : 0];
+            end
+        end
+    end // reg_update
+
+
+  // Map core inputs and outputs to the wrapper registers.
+  always @*
+    begin : mapping
+      core_initalize = ctrl_reg[SIPHASH_BIT_INITIALIZE];
+      core_compress  = ctrl_reg[SIPHASH_BIT_COMPRESS];
+      core_finalize  = ctrl_reg[SIPHASH_BIT_FINALIZE];
+      core_c         = param_reg[(SIPHASH_START_C + SIPHASH_SIZE_C) : SIPHASH_START_C];
+      core_d         = param_reg[(SIPHASH_START_D + SIPHASH_SIZE_D) : SIPHASH_START_D];
+      core_k         = {key0_reg, key1_reg, key2_reg, key3_reg};     
+      core_mi        = {mi0_reg, mi1_reg};
+    end
+
+  
+  //----------------------------------------------------------------
+  // register update control logic.
+  //----------------------------------------------------------------
+  always @*
+    begin : reg_ctrl
+      // Default assignments
+      read_data_out       = 32'h00000000;
+      read_data_valid_out = 1'b0;
+      error_out           = 1'b0;
+
+      ctrl_new            = 3'b000;
+      ctrl_we             = 1'b0;
+      param_new           = 8'h00;
+      param_we            = 1'b0;
+      
+      key0_new            = 32'h00000000;
+      key0_we             = 1'b0;
+      key1_new            = 32'h00000000;
+      key1_we             = 1'b0;
+      key2_new            = 32'h00000000;
+      key2_we             = 1'b0;
+      key3_new            = 32'h00000000;
+      key3_we             = 1'b0;
+      
+      mi0_new             = 32'h00000000;
+      mi0_we              = 1'b0;
+      mi1_new             = 32'h00000000;
+      mi1_we              = 1'b0;
+      
+      word0_new           = 32'h00000000;
+      word0_we            = 1'b0;
+      word1_new           = 32'h00000000;
+      word1_we            = 1'b0;
+      
+      if (cs)
+        begin
+          if (wr_rd)
+            begin
+              // Write operation.
+              case (addr)
+
+                SIPHASH_ADDR_CTRL:
+                  begin
+                    ctrl_new = write_data[2 : 0];
+                    ctrl_we  = 1'b1;
+                  end
+                
+                SIPHASH_ADDR_PARAM:
+                  begin
+                    param_new = write_data[7 : 0];
+                    param_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY0:
+                  begin
+                    key0_new = write_data;
+                    key0_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY1:
+                  begin
+                    key1_new = write_data;
+                    key1_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY2:
+                  begin
+                    key2_new = write_data;
+                    key2_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY3:
+                  begin
+                    key3_new = write_data;
+                    key3_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_MI0:
+                  begin
+                    mi0_new = write_data;
+                    mi0_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_MI1:
+                  begin
+                    mi1_new = write_data;
+                    mi1_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_WORD0:
+                  begin
+                    word0_new = write_data;
+                    word0_we  = 1'b1;
+                  end
+
+                SIPHASH_ADDR_WORD1:
+                  begin
+                    word1_new = write_data;
+                    word1_we  = 1'b1;
+                  end
+                
+                default:
+                  begin
+                    error_out = 1;
+                  end
+              endcase // case (addr)
+            end
+
+          else
+            begin
+              // Read operation.
+              case (addr)
+                SIPHASH_ADDR_CTRL:
+                  begin
+                    read_data_out       = {29'h00000000, ctrl_reg};
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_STATUS:
+                  begin
+                    read_data_out       = {30'h00000000, core_ready, core_siphash_word_valid};
+                    read_data_valid_out = 1'b1;
+                  end
+                
+                SIPHASH_ADDR_PARAM:
+                  begin
+                    read_data_out       = {24'h000000, param_reg};
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY0:
+                  begin
+                    read_data_out       = key0_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY1:
+                  begin
+                    read_data_out       = key1_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY2:
+                  begin
+                    read_data_out       = key2_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_KEY3:
+                  begin
+                    read_data_out       = key3_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_MI0:
+                  begin
+                    read_data_out       = mi0_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_MI1:
+                  begin
+                    read_data_out       = mi1_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_WORD0:
+                  begin
+                    read_data_out       = mi0_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+
+                SIPHASH_ADDR_WORD1:
+                  begin
+                    read_data_out       = mi1_reg;
+                    read_data_valid_out = 1'b1;
+                  end
+                
+                default:
+                  begin
+                    error_out = 1;
+                  end
+              endcase // case (addr)
+            end
+        end
+    end
+  
+endmodule // siphash
+
+//======================================================================
+// EOF siphash_core.v
+//======================================================================
---
+++ src/tb_siphash.v
@@ -0,0 +1,189 @@
+//======================================================================
+//
+// tb_siphash.v
+// ------------
+// Testbench for the SipHash core wrapper.
+//
+//
+// Copyright (c) 2012, Secworks Sweden AB
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or 
+// without modification, are permitted provided that the following 
+// conditions are met: 
+// 
+// 1. Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer. 
+// 
+// 2. Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in 
+//    the documentation and/or other materials provided with the 
+//    distribution. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//======================================================================
+
+//------------------------------------------------------------------
+// Compiler directives.
+//------------------------------------------------------------------
+`timescale 1ns/10ps
+
+module tb_siphash();
+  
+  //----------------------------------------------------------------
+  // Internal constant and parameter definitions.
+  //----------------------------------------------------------------
+  parameter CLK_HALF_PERIOD = 2;
+
+  
+  //----------------------------------------------------------------
+  // Register and Wire declarations.
+  //----------------------------------------------------------------
+  // Cycle counter.
+  reg [31 : 0] cycle_ctr;
+
+  // Clock and reset.
+  reg tb_clk;
+  reg tb_reset_n;
+
+  // DUT connections.
+  reg           tb_cs;
+  reg           tb_wr_rd;
+  reg [3 : 0]   tb_addr;
+  reg [31 : 0]  tb_write_data;
+  wire [31 : 0] tb_read_data;
+  wire          tb_read_data_valid;
+  wire          tb_error;
+
+  
+  //----------------------------------------------------------------
+  // siphash device under test.
+  //----------------------------------------------------------------
+  siphash dut(
+              // Clock and reset.
+              .clk(tb_clk),
+              .reset_n(tb_reset_n),
+                
+              .cs(tb_cs),
+              .wr_rd(tb_wr_rd),
+              .addr(tb_addr),
+              .write_data(tb_write_data),
+              .read_data(tb_read_data),
+              .read_data_valid(tb_read_data_valid),
+              .error(tb_error)
+             );
+  
+
+  //----------------------------------------------------------------
+  // clk_gen
+  // Clock generator process. 
+  //----------------------------------------------------------------
+  always 
+    begin : clk_gen
+      #CLK_HALF_PERIOD tb_clk = !tb_clk;
+    end // clk_gen
+
+  
+  //--------------------------------------------------------------------
+  // dut_monitor
+  // Monitor for observing the inputs and outputs to the dut.
+  // Includes the cycle counter.
+  //--------------------------------------------------------------------
+  always @ (posedge tb_clk)
+    begin : dut_monitor
+      cycle_ctr = cycle_ctr + 1;
+
+      $display("cycle = %8x:", cycle_ctr);
+    end // dut_monitor
+
+
+  //----------------------------------------------------------------
+  // dump_inputs
+  // Dump the internal SIPHASH state to std out.
+  //----------------------------------------------------------------
+  task dump_inputs();
+    begin
+      $display("Inputs:");
+      $display("");
+    end
+  endtask // dump_inputs
+
+
+  //----------------------------------------------------------------
+  // dump_outputs
+  // Dump the outputs from the SipHash to std out.
+  //----------------------------------------------------------------
+  task dump_outputs();
+    begin
+      $display("Outputs:");
+      $display("");
+    end
+  endtask // dump_inputs
+
+
+  //----------------------------------------------------------------
+  // dump_state
+  // Dump the internal SIPHASH state to std out.
+  //----------------------------------------------------------------
+  task dump_state();
+    begin
+      $display("Internal state:");
+      $display("");
+    end
+  endtask // dump_state
+
+  
+  //----------------------------------------------------------------
+  // siphash_test
+  // The main test functionality. 
+  //----------------------------------------------------------------
+  initial
+    begin : siphash_test
+      $display("   -- Testbench for siphash wrapper started --");
+
+      // Set clock, reset and DUT input signals to 
+      // defined values at simulation start.
+      tb_cs         = 1'b0;
+      tb_wr_rd      = 1'b0;
+      tb_addr       = 4'h0;
+      tb_write_data = 32'h00000000;
+      
+      cycle_ctr    = 0;
+      tb_clk       = 0;
+      tb_reset_n   = 0;
+      // dump_state();
+      
+      // Wait ten clock cycles and release reset.
+      #(20 * CLK_HALF_PERIOD);
+      @(negedge tb_clk)
+      tb_reset_n = 1;
+      // dump_state();
+      
+      // Wait some cycles.
+      #(200 * CLK_HALF_PERIOD);
+      $display("Processing done..");
+      // dump_state();
+      // dump_outputs();
+      
+      // Finish in style.
+      $display("siphash simulation done.");
+      $finish;
+    end // siphash_test
+  
+endmodule // tb_siphash
+
+//======================================================================
+// EOF tb_siphash.v
+//======================================================================
